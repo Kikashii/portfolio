2ccab0dd69eed5800333a3c2b5fa872aebf11d8b
Rafid Nahiyan Farabi <71763584+Kikashii@users.noreply.github.com>
2024-01-08 23:50:25 +0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -113,7 +113,7 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: '@rafid.nahiyan.farabi.85', // to hide blog section, keep it empty
+    username: 'rafid.nahiyan.farabi.85', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
